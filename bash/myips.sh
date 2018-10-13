002a9443b79dc958c6e57c40c2aa9bf412abb5a3
DJ <D.Dainty@outlook.com>
2018-10-13 19:01:36 -0400

    made myips.sh executable

--- bash/myips.sh
+++ bash/myips.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# 
+# print the ipv4 address on enp0s3
 echo -n "enp0s3: "
 ifconfig enp0s3 | grep 'inet ' |awk '{print $2}'|sed 's,/.*,,'
 
+# verifies internet connection and prints public ip
 echo -n "External IP: "
 curl icanhazip.com
 echo ''
